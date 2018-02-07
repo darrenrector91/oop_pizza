1bdff3f47d8f591e3a08fa473b4fea36460ed670
darrenrector91 <darrenrector91@gmail.com>
2018-02-07 13:57:14 -0600

    pizza class working

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 // console.log('client sourced!');
 
-class Pizza {
-    constructor(ingredient = 'cheese') {
-        this.noise = noise;
-    }
+// class Restaurant {
+//     constructor() {
+//         this.pizza = pizza;
+//         this.order =order;
+//     }
 
+// }
+
+class Pizza {
+    constructor(ingredient1 = "cheese", ingredient2, size, crust) {
+        this.ingredient1 = ingredient1;
+        this.ingredient2 = ingredient2;
+        this.size = size;
+        this.crust = crust;
+    }
 }
+
+let pepperoniPizza = new Pizza('pepperoni', 'sausage', 14, 'original');
+console.log(pepperoniPizza);
+
 
 class Order {
     constructor() {
-        
+
     }
 }
